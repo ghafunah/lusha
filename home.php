424ae453e16d0eff3551a495d9e9424bf37db814
ghafunah <ghafred@gmail.com>
2014-04-08 14:31:21 +0300

    c2

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html >
 <head>
-<title>LUSHA</title>
+<title>Home</title>
  <link href="menu.css" rel="stylesheet" type="text/css" />
  <link href="lusha_css.css" rel="stylesheet" type="text/css" />
 </head>
@@ -12,9 +12,12 @@
 <?php include('logo.php'); ?>
 <?php include('menu.php'); ?>
 <div class="banner">
-<?php include("homebanner.php") ?></div>
+<?php include("homebanner.php") ?>
+</div>
+
 <div class="contentwrap">
 <div class="left">
+
 	 <div id="paid">Paid Advertisement 1 goes here</div>
  	 
     <div id="paid">Paid Advertisement 2 goes here </div>
@@ -27,15 +30,15 @@
 <div class="center">
   <p><strong>LUMBOKA  SCHOOLS ALUMNI ASSOCIATION-LUSHA</strong><br />
     <br />
-    Is a NON-political registered association , A brainchild of Cyrus Kong'ani and Lillian Nkech in early 2011, this came about after long deliberations on the need for people to remember where they have come from. This Facebook group and collection of contacts of several people were initiated to be used to reach out to people to sell out the idea.A meeting was convened where an interim leadership committee was formed comprising of:</p>
+    Is a NON-political registered association , a brainchild of Cyrus Kong'ani and Lillian Nkech in early 2011, this came about after long deliberations on the need for people to remember where they have come from. This Facebook group and collection of contacts of several people were initiated to be used to reach out to people to sell out the idea.A meeting was convened where an interim leadership committee was formed comprising of:</p>
   <ul>
     <li>David Fwaro as the interim chairman</li>
     <li>Eric Nakitare as the treasurer</li>
     <li>Godfrey Masika Nyukuri as the Organizing  secretary</li>
     <li>Lillian Kong'ani as the secretary.</li>
 </ul>
-  <p>The  members present agreed to come up with The Name, the objectives, and draft  constitution, draft strategic plan and membership fee.<br />
-    They agreed that the objectives should  revolve around Academics, Health, Social and civic awareness, Environment and  the development of the area. </p>
+  <p>The  members present agreed to come up with the name, the objectives, and draft  constitution, draft strategic plan and membership fee.<br />
+    They agreed that the objectives should  revolve around Academics, Health, Social and civic awareness, environment and  the development of the area. </p>
   
   </div>
 <div class="right">
